647202b14ed70afb41682e8885beda3dfbf73446
Nick Ocampo <nick@wehatenick.com>
2015-01-03 18:45:26 -0800

    added gallery functionality

---
+++ app/scripts/image-selector.controller.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('imageSelector', [])
+	.controller('ImageSelectorCtrl', function ($scope) {
+		$scope.selectedImage = {
+			path:  'images/screenshots/valid.png'
+		};
+		$scope.images = [
+			{ path: 'images/screenshots/valid.png' },
+			{ path: 'images/screenshots/facebook-share.png' },
+			{ path: 'images/screenshots/homescreen.png' },
+			{ path: 'images/screenshots/invalid.png' }
+		];
+		$scope.selectImage = function (path) {
+			if (path) {
+				$scope.selectedImage.path = path;
+			}
+		};
+	});
